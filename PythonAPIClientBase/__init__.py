e7f15eb440200bfaf8ab584499cab972be37765b
Robert Metcalf <rmetcalf9@googlemail.com>
2020-11-14 12:11:24 +0000

    Making exception public

--- PythonAPIClientBase/__init__.py
+++ PythonAPIClientBase/__init__.py
@@ -1,6 +1,6 @@
 # This package contains generic base classes for APIClients
 
-from .APIClientBase import APIClientBase
+from .APIClientBase import APIClientBase, APIClientException
 from .LoginSession import LoginSession, NullLoginSession
 from ._version import get_versions
 __version__ = get_versions()['version']
